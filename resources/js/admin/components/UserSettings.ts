1777d1543aa0611a025f1106eac5f75aeb9facc0
BlindWizard <blindwizard@yandex.ru>
2020-04-02 09:05:46 +1000

    SB-32

--- resources/js/admin/components/UserSettings.ts
+++ resources/js/admin/components/UserSettings.ts
@@ -12,7 +12,7 @@ import {ApiToken} from "../models/ApiToken";
                             <h2>API Settings</h2>
                         </div>
                         <div class="cell">
-                            <table>
+                            <table v-if="tokens.length > 0">
                                 <tr>
                                     <th>Name</th>
                                     <th>Token</th>
@@ -24,7 +24,7 @@ import {ApiToken} from "../models/ApiToken";
                         </div>
                         <div class="cell">
                             <input type="text" placeholder="Token name">
-                            <button :class="bem('button').add('primary').classes()">Create</button>
+                            <button :class="bem('button').add('primary').classes()" @click="addToken">Create</button>
                         </div>
                     </div>
                 </div>
